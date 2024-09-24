95e79290eb6040b933248493459fd617c5d66183
Asad Ehsan <asad.ehsan360@gmail.com>
2024-09-24 10:36:45 +0530

    fixing spinning down of backend

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
 import "./App.css";
 import Home from "./screens/Home";
-import axios from 'axios';
 import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css'
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
-
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Login from "./screens/Login";
 import Signup from "./screens/Signup";
 import { CartProvider } from "./components/ContextReducer";
 import MyOrder from "./screens/MyOrder.js";
-
-const url = `https://foodieease-backend.onrender.com/`;
-const interval = 30000; // Interval in milliseconds (30 seconds)
-
-function reloadWebsite() {
-  axios.get(url)
-    .then(response => {
-      console.log(`Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`);
-    })
-    .catch(error => {
-      console.error(`Error reloading at ${new Date().toISOString()}:`, error.message);
-    });
-}
 
 
 setInterval(reloadWebsite, interval);
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+const express =require('express');
+const axios = require('axios');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,5 +12,19 @@ root.render(
     <App />
   </React.StrictMode>
 );
+const url = `https://foodieease-backend.onrender.com/`;
+const interval = 30000; // Interval in milliseconds (30 seconds)
+
+function reloadWebsite() {
+  axios.get(url)
+    .then(response => {
+      console.log(`Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`);
+    })
+    .catch(error => {
+      console.error(`Error reloading at ${new Date().toISOString()}:`, error.message);
+    });
+}
+
+setInterval(reloadWebsite, interval);
 
 reportWebVitals();
